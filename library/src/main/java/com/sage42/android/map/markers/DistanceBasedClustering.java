6d41e68f06929033cc6ba3a216c22961032cdc4d
Corey Scott <corey.scott@gmail.com>
2013-06-07 12:43:25 +0800

    added missing license headers and file

--- library/src/main/java/com/sage42/android/map/markers/DistanceBasedClustering.java
+++ library/src/main/java/com/sage42/android/map/markers/DistanceBasedClustering.java
@@ -9,7 +9,21 @@ import com.google.android.gms.maps.model.LatLng;
 import com.sage42.android.map.DistanceUtils;
 
 /**
- * https://developers.google.com/maps/articles/toomanymarkers#distancebasedclustering
+ * Copyright (C) 2013- Sage 42 App Sdn Bhd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Reference: https://developers.google.com/maps/articles/toomanymarkers#distancebasedclustering
  * 
  * @author Corey Scott (corey.scott@sage42.com)
  *
